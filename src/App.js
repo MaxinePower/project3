e9b33a3eac5cbae3e98f40637e1b79331f9ba1d0
Maxine <maxine@power.sh>
2022-03-14 20:44:51 -0400

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
+
+  // App.js stuff
+
+  // create a useState for catData to keep track of the api results for all the cats pictures
+
+  // create another usestate called numOfCats to track how many kitties the user wants to see
+
+  // once user has selected a number of KitCats run a side effect to use numOfCats in an api call
+    // take the data from that api call and update catData or display error of some sort
+    // if its not an error pass catData as a prop to Gallery
+  
+  // Form.js stuff
+
+  // create a user select for picking the number of cats they want to see
+    // grab that selected value with an event listener on form submit and then update state for numOfCats
+
+  // Gallery.js stuff
+
+  // use map to map over that cat array!
+    // return KitCatImg component that youve passed the url from the cat data to as a prop
+
+  // KitCatImg.js stuff
+  
+  // return an img of the cat ! so it can render on the page
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/*  */}
     </div>
   );
 }
